b5c67b4953af1a0bb14a08feac10750d21746086
Forest <forest.zhou21@ycdsbk12.ca>
2019-05-29 11:10:23 -0400

    stuff

--- tmp3.py
+++ tmp3.py
@@ -3,8 +3,11 @@ import arcade
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+player_x = 60
+player_y = 0
 jump_h = 0
 shift = 0
+up = -20
 
 hit_d = False
 hit_l = False
@@ -14,77 +17,73 @@ up_pressed = False
 down_pressed = False
 left_pressed = False
 right_pressed = False
-jump = False
+
+start = False
 
 height_increments = [38, 72, 102, 128, 150, 168, 182]
-block_height = []
-block_left_side = []
-block_right_side = []
+block_height = [38, 110, 212]
+block_left_side = [160, 360, 230]
+block_right_side = [240, 440, 310]
 
 
 def on_update(delta_time):
-    global up_pressed, down_pressed, left_pressed, right_pressed, player_x, player_y
-    global jump, hit_d, hit_l, hit_r, jump_h, up, down
-
-    for i in range(len(block_left_side)):
-        for n in range(len(height_increments)):
-            if block_left_side[i] < player_x < block_right_side[i] and jump_h == height_increments[n]:
-                hit_d = True
-            elif player_y + jump_h == 0:
-                hit_d = True
-    
-    if player_y + jump_h <= 0:
-        hit_d = True
-    elif 160 < player_x < 240 and jump_h + player_y == 38:
-        hit_d = True
-    elif 360 < player_x < 440 and jump_h + player_y == 110:
-        hit_d = True
-    elif 230 < player_x < 310 and jump_h + player_y == 212:
-        hit_d = True
-    else:
-        hit_d = False
+    global up_pressed, left_pressed, right_pressed, player_x, start
 
     if up_pressed:
-        if not jump:
-            jump = True
-        else:
-            pass
+        start = True
 
-    if hit_d and jump:
-        print("yes")
+    if left_pressed and not hit_l and start:
+        player_x -= 8
 
-    if jump:
-        if hit_d and up > 0:
-            up = -20
-            player_y += jump_h
-            jump_h = 0
-        down = 0
-        jump_h = 0.5 * -up ** 2 + 200
-        up += 1
+    if right_pressed and not hit_r and start:
+        player_x += 8
 
-        print(player_y, player_x, jump_h)
+    if start:
+        check_hit()
+        jumping()
+
+
+def check_hit():
+    global hit_d, hit_l, hit_r, player_x, player_y, jump_h
+
+    hit_d = False
+    hit_r = False
+    hit_l = False
+
+    if player_x <= 60:
+        hit_l = True
+    if player_x >= 740:
+        hit_r = True
+    for i in range(len(block_left_side)):
+        if block_left_side[i] < player_x < block_right_side[i] and jump_h + player_y == block_height[i]:
+            hit_d = True
+        elif player_y + jump_h <= 0:
+            hit_d = True
 
     if player_y < 0:
         player_y = 0
 
-    if left_pressed:
-        player_x -= 8
 
-    if right_pressed:
-        player_x += 8
+def jumping():
+    global hit_d, jump_h, player_y, up
+
+    if hit_d and up > 0:
+        up = -20
+        player_y += jump_h
+        jump_h = 0
+
+    jump_h = 0.5 * -up ** 2 + 200
+    up += 1
+
+    print(player_y, player_x, jump_h)
 
 
-# Create a value that our snow_person1_x will start at.
-player_x = 50
-player_y = 0
-up = -20
-down = 0
+def new_platforms():
+    pass
 
 
-def platforms(x, y):
-    arcade.draw_rectangle_filled(200, 19, 80, 38, arcade.color.BLACK)
-    arcade.draw_rectangle_filled(350, 57, 80, 38, arcade.color.BLACK)
-    arcade.draw_rectangle_filled(275, 57, 80, 38, arcade.color.BLACK)
+def menu():
+    pass
 
 
 def draw_snow_person(x, y):
@@ -104,13 +103,14 @@ def draw_snow_person(x, y):
 
 
 def on_draw():
-    global player_x, player_y
+    global player_x, player_y, jump_h, shift
     arcade.start_render()
 
-    draw_snow_person(player_x, player_y + jump_h)
-    arcade.draw_rectangle_filled(200, 19, 80, 38, arcade.color.BLACK)
-    arcade.draw_rectangle_filled(400, 91, 80, 38, arcade.color.BLACK)
-    arcade.draw_rectangle_filled(270, 193, 80, 38, arcade.color.BLACK)
+    draw_snow_person(player_x, player_y + jump_h - shift)
+
+    arcade.draw_rectangle_filled(200, 19 - shift, 80, 38, arcade.color.BLACK)
+    arcade.draw_rectangle_filled(400, 91 - shift, 80, 38, arcade.color.BLACK)
+    arcade.draw_rectangle_filled(270, 193 - shift, 80, 38, arcade.color.BLACK)
 
 
 def on_key_press(key, modifiers):
